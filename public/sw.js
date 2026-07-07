4a2c74182950339aa7d4b103fb07c804fea0b085
efy26 <eliezerefoyaefy2018@gmail.com>
2026-07-06 22:00:24 -0400

    Ajustement de  pwa

--- public/sw.js
+++ public/sw.js
@@ -2,19 +2,28 @@ const CACHE = "informez-v1";
 
 const urls = [
     "/",
+    "/manifest.json",
     "/css/global.css",
     "/js/global.js",
-    "/assets/logo-192.png",
-    "/assets/logo-512.png"
+    "/assets/logo-192.jpeg",
+    "/assets/logo-512.jpeg"
 ];
 
 self.addEventListener("install", event => {
     event.waitUntil(
         caches.open(CACHE).then(cache => cache.addAll(urls))
     );
+
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+    event.waitUntil(clients.claim());
 });
 
 self.addEventListener("fetch", event => {
+    if (event.request.method !== "GET") return;
+
     event.respondWith(
         caches.match(event.request)
             .then(response => response || fetch(event.request))
